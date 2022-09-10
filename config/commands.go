4eeb8d12ea4d86412afd239689e57b62f7c39c88
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-09-09 23:56:21 -0300

    Updates descriptions.

--- config/commands.go
+++ config/commands.go
@@ -15,7 +15,10 @@ const (
 )
 
 var (
-	descriptions map[string]string
+	descriptions  map[string]string
+	inactives     = fmt.Sprintf("Returns the list of inactive users in days period. Usage: /%s 30", Inactives)
+	kickInactives = fmt.Sprintf("Removes all inactive users from group in a time period. Usage: /%s 30", KickInactives)
+	setWelcome    = fmt.Sprintf("Saves a new welcome message. Usage: /%s <text>", SetWelcome)
 )
 
 // GetDescriptions returns a map of descriptions on existing commands.
@@ -27,9 +30,9 @@ func init() {
 	descriptions = map[string]string{
 		Start:         helpDescription,
 		Help:          helpDescription,
-		Inactives:     fmt.Sprintf("Returns the list of inactive users in a time period. Usage: /%s 30d", Inactives),
-		KickInactives: "This is the kick inactive command",
+		Inactives:     inactives,
+		KickInactives: kickInactives,
 		Welcome:       "Shows the welcome message.",
-		SetWelcome:    fmt.Sprintf("Saves a new welcome message. Usage: /%s <text>", SetWelcome),
+		SetWelcome:    setWelcome,
 	}
 }
