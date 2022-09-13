91e19f4e48c0069448070f6b3f0de0b7dbd435d2
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-09-13 05:05:19 -0300

    Uses repository for welcome message.

--- services/welcome.go
+++ services/welcome.go
@@ -6,18 +6,14 @@ import (
 	"github.com/ariel17/golang-telegram-group-manager/config"
 )
 
-var (
-	welcomeMessage = map[int64]string{}
-)
-
 // SetWelcome saves the welcome message to show in the group.
 func SetWelcome(text string, chatID int64) {
-	welcomeMessage[chatID] = text
+	repository.SetWelcomeForChat(chatID, text)
 }
 
 // GetWelcome returns the saved welcome message to show.
 func GetWelcome(chatID int64) string {
-	text, exists := welcomeMessage[chatID]
+	text, exists := repository.GetWelcomeForChat(chatID)
 	if !exists {
 		return fmt.Sprintf("Hello! 👋 Welcome message is empty. You need to set one with /%s <text>", config.SetWelcome)
 	}
